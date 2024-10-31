cb47ad6d357705a0ed25151e12bf7ffb64a980d3
Prithvi <prithvin19@gmail.com>
2024-10-31 19:54:16 +0530

    mail wip

--- components/faq/FaqForm.tsx
+++ components/faq/FaqForm.tsx
@@ -1,12 +1,86 @@
+"use client"
 import React from 'react'
+import toast from "react-hot-toast";
 
 export const FaqForm = () => {
+    
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+// console.log("data", e)
+        try {
+            const formData = new FormData();
+            // formData.append("message", message);
+
+            const response = await fetch("/api/uploadS3", {
+                method: "POST",
+                body: formData,
+            });
+
+            const res = await fetch("/api/sendMail", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    recipientEmail: `admin@example.com`,
+                    subject: "New Message from User",
+                    // message: `User Name: ${userName}\nMessage: ${message}`,
+                }),
+            });
+
+            if (!response.ok && !res.ok) {
+                throw new Error("Failed to upload file");
+            }
+
+            const data = await response.json();
+            const fileUrl = data.fileUrl;
+            // console.log("uploading....");
+
+            // const result = await uploadFile({
+            //     variables: { fileUrl, filename: file.name, userId },
+            // });
+
+            // console.log('File uploaded successfully:', response);
+            // call query for new files
+            if (response && res) {
+                toast.success("We will get back shortly!", {
+                    position: "top-right",
+                    duration: 3000,
+                    style: {
+                        border: "1px solid #65a34e",
+                        padding: "16px",
+                        color: "#65a34e",
+                    },
+                    iconTheme: {
+                        primary: "#65a34e",
+                        secondary: "#FFFAEE",
+                    },
+                });
+            
+            }
+        } catch (error: any) {
+            console.error("Error sending message", error);
+            toast.error("Error sending message", {
+                position: "top-right",
+                duration: 3000,
+                style: {
+                    border: "1px solid #EB1C23",
+                    padding: "16px",
+                    color: "#EB1C23",
+                },
+                iconTheme: {
+                    primary: "#EB1C23",
+                    secondary: "#FFFAEE",
+                },
+            });
+        } 
+    };
     return (
         <div className='w-full sm:w-1/2 mx-auto mt-10 bg-primary p-10 space-y-6'>
             <h3 className='text-xl font-semibold text-white text-center w-full capitalize'>Need any help?</h3>
             <h4 className=' text-white '>We will get back in few hours incase of any queries!</h4>
             {/* Connect form */}
-            <form >
+            <form onSubmit={handleSubmit}>
                
                 {/* Address */}
                 <div className="mb-6">
